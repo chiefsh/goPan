6dd6d91c03ea5b5a6699d24a2bb04b1ab8b167ab
chiefsh <chief_victo@163.com>
2020-06-01 20:23:07 +0800

    添加文件下载功能

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	http.HandleFunc("/file/upload", handler.UploadHandler)
 	http.HandleFunc("/file/upload/suc", handler.UploadSucHandler)
 	http.HandleFunc("/file/meta", handler.GetFileMetaHandler)
+	http.HandleFunc("/file/download", handler.DownloadHandler)
 	fmt.Println("server start at: localhost:8080")
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
